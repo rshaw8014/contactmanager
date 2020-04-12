32c78f2dc27075afa46d411ee957c35a88ef5599
Rahul Kumar <rshaw8014@gmail.com>
2020-04-12 20:02:53 +0530

    Updates

---
+++ precache-manifest.10d526dfe57bc38ca0e358896c1087f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03251cbf40d17323b511ed2def49c5b0",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "11bbd3c2838ceff49b35",
+    "url": "/contactmanager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7dce7a98ce075c4ec028",
+    "url": "/contactmanager/static/css/main.8976b6df.chunk.css"
+  },
+  {
+    "revision": "11bbd3c2838ceff49b35",
+    "url": "/contactmanager/static/js/2.274677b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/contactmanager/static/js/2.274677b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dce7a98ce075c4ec028",
+    "url": "/contactmanager/static/js/main.f869813f.chunk.js"
+  },
+  {
+    "revision": "2b43dc7d0ef5c78fcd0f",
+    "url": "/contactmanager/static/js/runtime-main.08b2bbf7.js"
+  }
+]);
